54373109a364cafaf3cf80149b48b55826d1ac7b
BenJD1205 <s.anh2209@gmail.com>
2023-12-28 17:40:46 +0700

    fix: cors for client

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,8 @@ app.use(compression());
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser());
 app.use(cors({
-    origin: '*'
+    origin: ['http://localhost:3000'],
+    credentials: true,
 }));
 
 swagger(app);
